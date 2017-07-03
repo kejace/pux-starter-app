3fc99e7e1c4db1be703af1341e7da6ca665d058a
kejace <kejace@gmail.com>
2017-07-03 14:54:15 -0400

    Now listening on 0.0.0.0

--- src/App/Config.js
+++ src/App/Config.js
@@ -2,5 +2,5 @@ exports.config = {
   title: 'Pux Starter App',
   public_path: process.env.NODE_ENV === 'production'
                ? '/dist/'
-               : 'http://localhost:8080/dist/'
+               : 'http://0.0.0.0:8080/dist/'
 }
